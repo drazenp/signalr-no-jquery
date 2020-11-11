7fd7d455b8679d78ed37637b9cf4156ed75a9f51
Dmitry <dmitry@cojam.ru>
2020-11-11 18:36:23 +0500

    0.1.14 RC

--- gulpfile.js
+++ gulpfile.js
@@ -5,28 +5,60 @@ const clean = require('gulp-clean')
 const rename = require('gulp-rename')
 const header = require('gulp-header')
 
+const browserify = require('browserify')
+const source = require('vinyl-source-stream');
+
 const path = require('path')
 
 const getPackageDir = packageName => path.dirname(require.resolve(`${packageName}/package.json`))
 
+const srcDir = path.join(__dirname, 'src')
+
 const destDir = path.join(__dirname, 'lib')
+
+const tempDir = path.join(__dirname, 'tmp')
 
 const signalRDestName = 'signalR.js'
 
 const signalRPath = path.join(getPackageDir('signalr'), 'jquery.signalR.min.js')
 
-const signalRDestPath = path.join(destDir, signalRDestName)
+const shimPath = path.join(srcDir, 'jQueryShim.js')
 
-gulp.task('clear-signalr-dest', () => pipe([
-	gulp.src(signalRDestPath, { read: false, allowEmpty: true }),
+const signalRTmpPath = path.join(tempDir, signalRDestName)
+
+gulp.task('tempDir:clear', () => pipe([
+	gulp.src(tempDir, { read: false, allowEmpty: true }),
 	clean()
 ]))
 
-gulp.task('get-signalr', () => pipe([
+gulp.task('destDir:clear', () => pipe([
+	gulp.src(tempDir, { read: false, allowEmpty: true }),
+	clean()
+]))
+
+gulp.task('shim:get', () => pipe([
+	gulp.src(shimPath),
+	gulp.dest(tempDir)
+]))
+
+gulp.task('signalr:get', () => pipe([
 	gulp.src(signalRPath),
 	header(`const jQueryShim = require('./jQueryShim');\n`),
 	rename(signalRDestName),
+	gulp.dest(tempDir)
+]))
+
+gulp.task('signalr:build', () => pipe([
+	browserify(signalRTmpPath).bundle(),
+	source(signalRDestName),
 	gulp.dest(destDir)
 ]))
 
-gulp.task('default', gulp.series('clear-signalr-dest', 'get-signalr'))
+gulp.task('typings:copy', () => pipe([
+	gulp.src(`${srcDir}/signalR.d.ts`),
+	gulp.dest(destDir)
+]))
+
+gulp.task('build', gulp.series('tempDir:clear', 'shim:get', 'signalr:get', 'signalr:build', 'tempDir:clear'))
+
+gulp.task('default', gulp.series('destDir:clear', 'build', 'typings:copy'))
